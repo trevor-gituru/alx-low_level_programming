f61d2f9a952c92e58c025eb3b842d346048bf0ad
Trevor Muriuki <giturutrevor@gmail.com>
2023-12-16 23:23:23 -0800

    T5-U

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point of the program
  *
@@ -9,11 +8,9 @@
  */
 int main(void)
 {
-    const char *message = "Programming is like building a multilingual puzzle";
-
+    const char *message = "\"Programming is like building a multilingual puzzle";
     /* Using the puts function to print the string followed by a new line */
     puts(message);
-
     /* End the program with the value 0 */
     return (0);
 }
