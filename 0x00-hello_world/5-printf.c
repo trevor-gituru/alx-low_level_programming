df9481017f1141aa4b72b73edb4f72d52ce02b13
Trevor Muriuki <giturutrevor@gmail.com>
2023-12-16 23:43:44 -0800

    T5-5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific string using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *message = "with proper grammar, but the outcome is a piece of art,\n";
+	/* Using the prints function to print the string followed by a new line */
+	printf("%s", message);
+	/* End the program with the value 0 */
+	return (0);
+}
