29be56fbd5adb34312f229a6a70b7042bfb0536b
Trevor Muriuki <giturutrevor@gmail.com>
2023-12-16 23:18:16 -0800

    T5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific string using the puts function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    const char *message = "Programming is like building a multilingual puzzle";
+
+    /* Using the puts function to print the string followed by a new line */
+    puts(message);
+
+    /* End the program with the value 0 */
+    return (0);
+}
