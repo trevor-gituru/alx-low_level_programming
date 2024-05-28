a3c128080842cc7272a0424846ee186e61f7373d
Trevor Gituru <giturutrevor@gmail.com>
2024-05-28 10:30:08 +0000

    Restructured

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,39 +6,8 @@
 #include <string.h>
 
 /* Function prototype */
-void prints(void);
+int _putchar(char c);
 void print_alphabet(void);
-
-/**
- * prints - Prints a character
- *
- * Return: Null
- */
-int _putchar(char c)
-{
-
-	/* Using the write function to print the character followed by a new line */
-	return write(STDOUT_FILENO, &c, 1);
-}
-
-/**
- * print_alphabet - Prints the alphabet
- *
- * Return: Null
- */
-void print_alphabet(void)
-{
-	char letter;
-        int n;
-
-        letter = 'a';
-        for (n = 0; n < 26; n++)
-        {
-                _putchar(letter);
-                letter = (char) (((int) letter) + 1);
-        }
-        _putchar('\n');
-
-}
+void print_alphabet_x10(void);
 
 #endif
