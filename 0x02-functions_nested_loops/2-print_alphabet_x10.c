65bceed018a0888b2fa5d77194ab3d9770af3248
Trevor Gituru <giturutrevor@gmail.com>
2024-05-28 12:13:53 +0000

    ./putchar.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times
+ *
+ * Return: Null
+ */
+void print_alphabet_x10(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		print_alphabet();
+	}
+}
