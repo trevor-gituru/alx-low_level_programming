707681a629c7a734af2e5bf1489455d50ea1686e
Trevor Gituru <giturutrevor@gmail.com>
2024-05-28 12:19:13 +0000

    ./putchar.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,6 +11,15 @@ void print_alphabet_x10(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		print_alphabet();
+		char letter;
+		int n;
+
+		letter = 'a';
+		for (n = 0; n < 26; n++)
+		{
+			_putchar(letter);
+			letter = (char) (((int) letter) + 1);
+		}
+		_putchar('\n');
 	}
 }
