1cddb1c07edaed93eec8e7523204f79e04823e22
Trevor Gituru <giturutrevor@gmail.com>
2024-05-29 07:05:59 +0000

    Task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - Evaluates for sign
+ * @n: Number
+ * Return: 1 (positive), 0 (zero), -1 (negative)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+}
