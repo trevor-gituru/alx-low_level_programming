91871366d4c7513f88bdb9e735f961cc46ba319f
Trevor Gituru <152320650+trevor-gituru@users.noreply.github.com>
2024-05-30 15:11:29 +0300

    Task 12: Create 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+  * print_number - prints an integer
+  * @n: integer to be printed
+  *
+  * Return: void
+  **/
+
+void print_number(int n)
+{
+	unsigned int k = n;
+
+	if (n < 0)
+	{
+		n *= -1;
+		k = n;
+		_putchar('-');
+	}
+
+	k /= 10;
+
+	if (k != 0)
+		print_number(k);
+
+	_putchar((unsigned int) n % 10 + '0');
+
+}
