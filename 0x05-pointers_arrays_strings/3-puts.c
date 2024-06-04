211ff5ee1197346c5770f2fbd2a9a4b164583b5e
Trevor Gituru <152320650+trevor-gituru@users.noreply.github.com>
2024-06-04 15:23:39 +0300

    Task 3: Create 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * _puts - prints a string to stdout
+  * @str: string to be printed
+  *
+  * Return: void
+  **/
+
+void _puts(char *str)
+{
+	puts(str);
+}
