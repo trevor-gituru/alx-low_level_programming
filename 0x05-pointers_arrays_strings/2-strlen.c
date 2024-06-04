74a8ae3e4b2d8381e4528ca3d7ba93a58625d1d3
Trevor Gituru <152320650+trevor-gituru@users.noreply.github.com>
2024-06-04 14:58:49 +0300

    Task 2: Create 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * _strlen - returns the length of a string.
+  * @s: string whose length is to be determined
+  *
+  * Return: integer value
+  **/
+
+int _strlen(char *s)
+{
+	int string_length = strlen(s);
+
+	return (string_length);
+}
