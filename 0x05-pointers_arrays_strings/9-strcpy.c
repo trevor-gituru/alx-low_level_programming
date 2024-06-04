32856e6c671e4198ce24e01c6c27c605367d7ee7
Trevor Gituru <152320650+trevor-gituru@users.noreply.github.com>
2024-06-04 15:53:49 +0300

    Task 9: Create  9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,14 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * *_strcpy - copies the string pointed to by src
+  * @dest: destination buffer
+  * @src: source address
+  * Return: character string
+  **/
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
