2a33856b8ad90cd30f9a6e94bc626edee242f75c
Trevor Gituru <giturutrevor@gmail.com>
2024-06-11 17:48:33 +0300

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,36 @@
+#include <string.h>
+#include "main.h"
+
+/**
+  * *_strncat - concatenating two strings
+  * @dest: destination param big enough to hold src
+  * @src: source
+  * @n: number of bytes to use
+  *
+  * Return: pointer to a character
+  **/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int src_len;
+	int dest_len;
+	int i;
+
+	src_len = 0;
+	dest_len = 0;
+	while (*(src + src_len) != '\0')
+	{
+		src_len++;
+	}
+	while (*(dest + dest_len) != '\0')
+	{
+		dest_len++;
+	}
+	for (i = 0; i < n && i < (98 - dest_len); i++)
+	{
+		*(dest + dest_len + i) = *(src + i);
+	}
+	*(dest + dest_len + src_len + 1) = '\0';
+
+	return (dest);
+}
