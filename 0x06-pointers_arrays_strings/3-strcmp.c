ab519d59dc292f2c0d6828e4d7dd14f751c0be6d
Trevor Gituru <giturutrevor@gmail.com>
2024-06-11 19:34:52 +0300

    Task 3: Rennovation

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,5 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
-	while ((*(s1 + i) == *(s2 + i)) && *(s1 + i) && *(s1 + i))
-	{
-		i++;
-	}
-	return (*(s1 + i) - *(s2 - i));
+	return (strcmp(s1, s2));
 }
