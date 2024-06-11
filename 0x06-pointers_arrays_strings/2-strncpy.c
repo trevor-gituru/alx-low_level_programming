fac2bc7f922a318155a2166035c82785ccae5ca9
Trevor Gituru <giturutrevor@gmail.com>
2024-06-11 18:44:58 +0300

    Task 2: Extended the function

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,5 +12,29 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	return (strncpy(dest, src, n));
+	int src_len;
+	int dest_len;
+	int i;
+
+	src_len = 0;
+	dest_len = 0;
+	while (*(src + src_len) != '\0')
+	{
+		src_len++;
+	}
+	while (*(dest + dest_len) != '\0')
+	{
+		dest_len++;
+	}
+	for (i = 0; i < n && src[i] != '\0' ; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	for ( ; i < n; i++)
+    {
+        *(dest + i) = '\0';
+    }
+    
+
+	return (dest);
 }
