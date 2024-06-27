5c7d462aca05223d333cd75aeb9f8d8ad46043b3
Trevor Gituru <giturutrevor@gmail.com>
2024-06-27 13:27:19 +0300

    Task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - execute a function given as a parameter
+ * @name: parameter that function ptr needs
+ * @f:function to execute
+ * Return: void
+ **/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		(*f)(name);
+	}
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,4 @@
+#ifndef _function_pointers_h
+#define _function_pointers_h
+void print_name(char *name, void (*f)(char *));
+#endif
